c5a0949cc2138298c86561e7fa857044bd297963
user-faiz <email2faizm@gmail.com>
2023-07-18 04:15:15 +0530

    add content section in index.js

--- index.js
+++ index.js
@@ -2,7 +2,17 @@ import BookCollection from './modules/bookcollection.js';
 import currentDate from './modules/currentDate.js';
 // Create a book collection instance
 const bookCollection = new BookCollection();
+// Navigation links
+const navLinks = document.getElementsByClassName('nav-link');
 
+// Content sections
+const sections = {
+  books: document.getElementById('books-section'),
+  'add-book': document.getElementById('add-book-section'),
+  'contact-info': document.getElementById('contact-info-section'),
+};
+
+});
 
 
 
