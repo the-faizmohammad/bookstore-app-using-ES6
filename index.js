981ffeed69c5c8bfd98a587cfeae50b3042c900e
user-faiz <email2faizm@gmail.com>
2023-07-18 04:12:43 +0530

    update index.css

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import BookCollection from './modules/bookcollection.js';
 import currentDate from './modules/currentDate.js';
+
+
 const currentDateElement = document.getElementById('current-date');
 currentDateElement.textContent = currentDate;
