fec022b69d35e445bc1b964e7376eec070d0d52e
user-faiz <email2faizm@gmail.com>
2023-07-18 04:13:48 +0530

    create bookcollection instance in index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import BookCollection from './modules/bookcollection.js';
 import currentDate from './modules/currentDate.js';
+// Create a book collection instance
+const bookCollection = new BookCollection();
+
+
 
 
 const currentDateElement = document.getElementById('current-date');
